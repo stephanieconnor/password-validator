86124d9447ea13e0d418d3999fe044bb94a8b5ab
stephanieconnor <stephanieconnor1@gmail.com>
2017-05-29 11:10:14 -0300

    EspressoTest added Button, inputField, and outputField added

---
+++ app/src/androidTest/java/com/stephanieconnor/passwordvalidator/EspressoTest.java
@@ -0,0 +1,13 @@
+package com.stephanieconnor.passwordvalidator;
+
+/**
+ * Created by stephanieconnor on 2017-05-29.
+ */
+
+public class EspressoTest {
+
+    onView(validateButton(R.id.button))
+
+
+
+}
