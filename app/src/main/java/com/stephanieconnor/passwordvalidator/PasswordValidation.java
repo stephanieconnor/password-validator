652ad0c1f4cef346ea55290beff2b837c470ad28
stephanieconnor <stephanieconnor1@gmail.com>
2017-05-29 00:47:40 -0300

    Added the methods notPassword() and length() to passwordValidator main

--- app/src/main/java/com/stephanieconnor/passwordvalidator/PasswordValidation.java
+++ app/src/main/java/com/stephanieconnor/passwordvalidator/PasswordValidation.java
@@ -10,4 +10,21 @@ public class PasswordValidation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_validation);
     }
+
+    //method that checks that the password != "password"
+    public int notPassword(String p){
+        if (!p.equals("password"))
+            return 1;
+        else
+            return 0;
+    }
+
+    //method that ensures that the length is greater than 8 char
+    public int length(String p){
+        if (p.length() > 8)
+            return 1;
+        else
+            return 0;
+    }
+
 }
