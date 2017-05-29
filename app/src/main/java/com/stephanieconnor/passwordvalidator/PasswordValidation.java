0066ebb0059c6d5a527d952e9077f1e764d9fd00
stephanieconnor <stephanieconnor1@gmail.com>
2017-05-29 01:46:47 -0300

    Added the methods to determine if the password has an uppercase letter, digit, and special char

--- app/src/main/java/com/stephanieconnor/passwordvalidator/PasswordValidation.java
+++ app/src/main/java/com/stephanieconnor/passwordvalidator/PasswordValidation.java
@@ -11,20 +11,79 @@ public class PasswordValidation extends AppCompatActivity {
         setContentView(R.layout.activity_password_validation);
     }
 
+    //goes through the methods to determine the number of tests passed
+    public int isValid(String p){
+
+        int test = 0;
+
+        if (notPassword(p))
+            test++;
+        if(length(p))
+            test++;
+        if(uppercase(p))
+            test++;
+        if(digit(p))
+            test++;
+        if(special(p))
+            test++;
+
+        return test;
+    }
+
+
+
     //method that checks that the password != "password"
-    public int notPassword(String p){
-        if (!p.equals("password"))
-            return 1;
-        else
-            return 0;
+    public boolean notPassword(String p){
+        return !p.equals("password");
     }
 
     //method that ensures that the length is greater than 8 char
-    public int length(String p){
-        if (p.length() > 8)
-            return 1;
-        else
-            return 0;
+    public boolean length(String p){
+        return p.length() > 8;
     }
 
+    //method that ensures that there is at least one Uppercase letter
+    public boolean uppercase(String p){
+
+        for (int i = 0; i < p.length(); i++){
+            if (Character.isUpperCase(p.charAt(i))) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    //method that ensures that there is at least one digit
+    public boolean digit(String p){
+
+        for (int i = 0; i < p.length(); i++) {
+            if (Character.isDigit(p.charAt(i)))
+                return true;
+
+        }
+
+        return false;
+    }
+
+    public boolean special(String p) {
+
+        //created a char array with all the special chars
+        char[] special;
+        special = new char[] { '"', ' ', '!', '#', '$', '%', '&', '(', ')',
+                '*', '+' , ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']',
+                '^', '_' , '`', '{', '|', '}', '~'};
+        char[] charArray = p.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = 0; j < special.length; j++ )
+                if (charArray[i] == special[j]) {
+                    return true;
+                }
+        }
+
+        return false;
+
+    }
 }
